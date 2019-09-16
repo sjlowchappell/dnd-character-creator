eba5e90c334837c1f2340367f0637df6ee03b90e
Sam Low-Chappell <sam@hackeryou.com>
2019-09-16 16:55:40 -0400

    Added a title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>DND Character Creator</h1>
+		</div>
+	);
 }
 
 export default App;
