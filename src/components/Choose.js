5f65458e355390bf531bf564bb1ed2d78d0daafd
Sam Low-Chappell <sam@hackeryou.com>
2019-09-19 12:36:26 -0400

    removed unnecessary divs

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -3,17 +3,15 @@ import React from 'react';
 function Choose({ choices }) {
 	return (
 		<div>
-			<div>
-				<h3>Choose {choices.choose} of the Following:</h3>
-				{choices.from.map(choice => {
-					return (
-						<li>
-							<label htmlFor="">{choice.name || `${choice.quantity} ${choice.item.name}`}</label>
-							<input type="checkbox" />
-						</li>
-					);
-				})}
-			</div>
+			<h3>Choose {choices.choose} of the Following:</h3>
+			{choices.from.map(choice => {
+				return (
+					<li>
+						<label htmlFor="">{choice.name || `${choice.quantity} ${choice.item.name}`}</label>
+						<input type="checkbox" />
+					</li>
+				);
+			})}
 		</div>
 	);
 }
